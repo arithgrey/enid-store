3b31c37bae4a89ad530bffe45e69ec0e8343451a
arithgrey <arithgrey@gmail.com>
2024-05-08 01:31:49 -0600

    [CHORE] deprected source is removed

--- enid/user/urls.py
+++ enid/user/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from user.views import UserValidatorViewSet
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 
 urlpatterns = [
-    path('exists/<str:email>', UserValidatorViewSet.as_view({'get': 'user_exists'}), name='exists'),
     path('', include(router.urls)),
 ]
